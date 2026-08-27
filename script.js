e95512cadfb8c2d96fda964e99199503a9ec92e0
Jhon-Av18 <jhon.avilas@uniagustiniana.edu.co>
2026-08-27 15:14:01 +0000

    change script

---
+++ script.js
@@ -0,0 +1,17 @@
+var texto = "hola"
+var numero = 180;
+var boleano =false;
+var lista = [ 1, 2, 3];
+var objeto = { "curso" : "curso web"}
+
+function mifuncion(parametro){
+    var texto;
+    if(parametro > texto){
+        texto = true
+    }else{
+        texto = false 
+    }
+    return texto
+}
+
+console.log(mifuncion(7))
