c2a296ff1c3d28b68cd2cd77adcab9ccb5467c09
Jhon-Av18 <jhon.avilas@uniagustiniana.edu.co>
2026-08-27 15:19:07 +0000

    for

--- script.js
+++ script.js
@@ -3,11 +3,16 @@ var numero = 180;
 var boleano =false;
 var lista = [ 1, 2, 3];
 var objeto = { "curso" : "curso web"}
+i= 0
 
 function mifuncion(parametro){
     var texto;
     if(parametro > texto){
         texto = true
+        for(let i=0 ; i<4;i++){
+            console.log("impresion:"{i})
+        }
+            
     }else{
         texto = false 
     }
